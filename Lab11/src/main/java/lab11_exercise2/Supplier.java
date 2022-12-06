7f8efe5cd18cefedbe56b2c611c45018f5e28909
gpott <gpott@LAPTOP-G3NAEUUG.surrey.ac.uk>
2022-12-06 12:33:10 +0000

    lab11 started

---
+++ Lab11/src/main/java/lab11_exercise2/Supplier.java
@@ -0,0 +1,25 @@
+package lab11_exercise2;
+
+public class Supplier {
+
+	private String name;
+	private String phone;
+
+	public Supplier(String name, String phone) {
+		this.name = name;
+		if (phone.matches("0[0-9]{6,10}")) {
+			this.phone = phone;
+		} else {
+			throw new IllegalArgumentException("invalid phone number");
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+}
